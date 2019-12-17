6830014639ed44e9c8aebd14ef709d3f11031dfe
Ruud <ruudniew@protonmail.com>
2019-12-17 20:46:51 +0100

    No more stupid passwords

--- cmd/sbapi/sbapi.go
+++ cmd/sbapi/sbapi.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"sbapi/customdb"
 	"sbapi/gin"
 )
@@ -9,11 +8,8 @@ import (
 func main() {
 	customDB := customdb.CustomDB{}
 
-	passwordBytes, _ := ioutil.ReadFile("./password")
-	pass := string(passwordBytes)
-
 	api := gin.API{}
 	api.EventService = &customDB
 
-	api.Start("", "3300", []string{"http://127.0.0.1:3400", "http://localhost:3200", "https://soundboard.ruudniew.com"}, pass)
+	api.Start("", "3300", []string{"http://127.0.0.1:3400", "http://localhost:3200", "https://soundboard.ruudniew.com"})
 }
